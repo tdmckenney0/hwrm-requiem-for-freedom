44517d490b8e5638c43d71ad1933c51905309430
tdmckenney0 <tmckenney7@outlook.com>
2015-03-17 20:53:57 -0400

    Readme and SymLink Tool
    Adds the Readme and Symlink batch script to create symbolic links to the
    data: directory.

---
+++ README.md
@@ -0,0 +1,10 @@
+Homeworld: Requiem for Freedom
+--------------------------------------
+
+Author: Tanner Mckenney (TheMadScienceGuy)
+
+With help of: Matthew Collins, Erik Mann
+
+Sub\Real Industries, 2015
+
+--------------------------------------
---
+++ tools/create_symlinks.bat
@@ -0,0 +1,13 @@
+cd ..
+mklink Homeworld2.big ..\Homeworld2.big
+mklink UpdateHomeworld2.big ..\UpdateHomeworld2.big
+mklink English.big ..\English.big
+mklink EnglishSpeech.big ..\EnglishSpeech.big
+mklink UpdateHW1Ships.big ..\UpdateHW1Ships.big
+mklink UpdateHW2Ships.big ..\UpdateHW2Ships.big
+mklink HW2Ships.big ..\HW2Ships.big
+mklink HW1Ships.big ..\HW1Ships.big
+mklink HWBackgrounds.big ..\HWBackgrounds.big
+mklink Music.big ..\Music.big
+mklink MusicHW1Campaign.big ..\MusicHW1Campaign.big
+mklink MusicHW2Campaign.big ..\MusicHW2Campaign.big

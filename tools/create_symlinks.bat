2163c74d32a2f5ec5d59a8b0a0b9e609ebc931b7
tdmckenney0 <tmckenney7@outlook.com>
2015-03-26 21:52:05 -0400

    Added Corvettes and Updated Fighters
    Fighters, Corvettes. Also added Battlecruiser production ability.

--- tools/create_symlinks.bat
+++ tools/create_symlinks.bat
@@ -1,13 +1,15 @@
+@echo off
+set /p url="Paste the path from your Homeworld Remastered data directory: "
 cd ..
-mklink Homeworld2.big ..\Homeworld2.big
-mklink UpdateHomeworld2.big ..\UpdateHomeworld2.big
-mklink English.big ..\English.big
-mklink EnglishSpeech.big ..\EnglishSpeech.big
-mklink UpdateHW1Ships.big ..\UpdateHW1Ships.big
-mklink UpdateHW2Ships.big ..\UpdateHW2Ships.big
-mklink HW2Ships.big ..\HW2Ships.big
-mklink HW1Ships.big ..\HW1Ships.big
-mklink HWBackgrounds.big ..\HWBackgrounds.big
-mklink Music.big ..\Music.big
-mklink MusicHW1Campaign.big ..\MusicHW1Campaign.big
-mklink MusicHW2Campaign.big ..\MusicHW2Campaign.big
+mklink Homeworld2.big %url%\Homeworld2.big
+mklink UpdateHomeworld2.big %url%\UpdateHomeworld2.big
+mklink English.big %url%\English.big
+mklink EnglishSpeech.big %url%\EnglishSpeech.big
+mklink UpdateHW1Ships.big %url%\UpdateHW1Ships.big
+mklink UpdateHW2Ships.big %url%\UpdateHW2Ships.big
+mklink HW2Ships.big %url%\HW2Ships.big
+mklink HW1Ships.big %url%\HW1Ships.big
+mklink HWBackgrounds.big %url%\HWBackgrounds.big
+mklink Music.big %url%\Music.big
+mklink MusicHW1Campaign.big %url%\MusicHW1Campaign.big
+mklink MusicHW2Campaign.big %url%\MusicHW2Campaign.big

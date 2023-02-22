a4acf44f11e58a74576af73a25e1c4c56a0ec5d1
Tanner Mckenney <tmckenney7@outlook.com>
2023-02-21 19:55:07 -0800

    Update Starting Fleets

--- scripts/startingfleets/vaygr00carriersonly.lua
+++ scripts/startingfleets/vaygr00carriersonly.lua
@@ -1,166 +1,205 @@
 PersistantData = {
-   StrikeGroups = {
-   },
-   Squadrons = {
-	{
-         type = "Vgr_ResourceCollector",
-         subsystems = {},
-         shiphold = {},
-         name = "",
-         number = 6,
-	},
-	{
-         type = "Vgr_Carrier",
-         subsystems = {
-            {
-                index = 0,
-                name = "VGR_C_PRODUCTION_CORVETTE",
-            },
-            {
-                index = 0,
-                name = "VGR_C_MODULE_RESEARCH", 
-            },
-            {
-                index = 0,
-                name = "VGR_C_MODULE_HYPERSPACE",
-            },
-            {
-                index = 0,
-                name = "VGR_C_SENSORS_ADVANCEDARRAY",
-            },
-         },
-         shiphold = {},
-         name = "",
-         number = 1,
-	},
-	{
-         type = "vgr_resourcecontroller",
-         subsystems = {},
-         shiphold = {},
-         name = "",
-         number = 1,
-	},
-	{
-         type = "vgr_assaultfrigate",
-         subsystems = {},
-         shiphold = {},
-         name = "",
-         number = 5,
-	},
-  {
-          type = "vgr_heavymissilefrigate",
-          subsystems = {},
-          shiphold = {},
-          name = "",
-          number = 5,
+  StrikeGroups = {
   },
-  {
-          type = "vgr_infiltratorfrigate",
-          subsystems = {},
-          shiphold = {},
-          name = "",
-          number = 2,
+  Squadrons = {
+    -- Carrier
+    {
+      type = "Vgr_Carrier",
+      subsystems = {
+        {
+          index = 0,
+          name = "VGR_C_PRODUCTION_CORVETTE",
+        },
+        {
+          index = 0,
+          name = "VGR_C_MODULE_RESEARCH",
+        },
+        {
+          index = 0,
+          name = "VGR_C_MODULE_HYPERSPACE",
+        },
+        {
+          index = 0,
+          name = "VGR_C_SENSORS_ADVANCEDARRAY",
+        },
+      },
+      shiphold = {},
+      name = "",
+      number = 1,
+    },
+    -- Utility
+    {
+      type = "Vgr_ResourceCollector",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 6,
+    },
+    {
+      type = "vgr_resourcecontroller",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 1,
+    },
+    -- Frigates
+    {
+      type = "vgr_assaultfrigate",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 5,
+    },
+    {
+      type = "vgr_heavymissilefrigate",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 5,
+    },
+    {
+      type = "vgr_infiltratorfrigate",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 2,
+    },
+    -- Fighters
+    {
+      type = "vgr_interceptor",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 5,
+    },
+    {
+      type = "vgr_bomber",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 5,
+    },
+    {
+      type = "vgr_lancefighter",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 5,
+    },
+    {
+      type = "vgr_scout",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 5,
+    },
+    -- Corvettes
+    {
+      type = "vgr_missilecorvette",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 4,
+    },
+    {
+      type = "vgr_lasercorvette",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 4,
+    },
+    {
+      type = "vgr_commandcorvette",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      number = 2,
+    },
   },
-	{
-         type = "vgr_interceptor",
-         subsystems = {},
-         shiphold = {},
-         name = "",
-         number = 5,
-	},
-  {
-         type = "vgr_scout",
-         subsystems = {},
-         shiphold = {},
-         name = "",
-         number = 5,
-	},
-		},
-   Research = {
-   [1] = {
+  Research = {
+    [1] = {
       name = "CorvetteTech",
       progress = 1,
     },
-	[2] = {
+    [2] = {
       name = "FrigateTech",
       progress = 1,
     },
-	[3] = {
+    [3] = {
       name = "ProbeSensorDisruption",
       progress = 1,
-	},
-	[4] = {
+    },
+    [4] = {
       name = "ProbeProxSensor",
       progress = 1,
     },
-	[5] = {
+    [5] = {
       name = "LanceBeams",
       progress = 1,
     },
-	[6] = {
+    [6] = {
       name = "PlasmaBombs",
       progress = 1,
     },
-	[7] = {
+    [7] = {
       name = "CorvetteLaser",
       progress = 1,
-	},
-	[8] = {
+    },
+    [8] = {
       name = "CorvetteCommand",
       progress = 1,
     },
-	[9] = {
+    [9] = {
       name = "PlatformHeavyMissiles",
       progress = 1,
     },
-	[10] = {
+    [10] = {
       name = "FrigateAssault",
       progress = 1,
     },
-	[11] = {
+    [11] = {
       name = "FrigateInfiltrationTech",
       progress = 1,
     },
-	[12] = {
+    [12] = {
       name = "FrigateGravity",
       progress = 1,
     },
-	[13] = {
+    [13] = {
       name = "FrigateCloak",
       progress = 1,
-	},
-	[14] = {
+    },
+    [14] = {
       name = "BattlecruiserIonWeapons",
       progress = 1,
     },
-	[15] = {
+    [15] = {
       name = "DestroyerGuns",
       progress = 1,
     },
-	[16] = {
+    [16] = {
       name = "AdvDestroyer",
       progress = 1,
     },
-	[17] = {
+    [17] = {
       name = "AncientHullEngineering",
       progress = 1,
-	},
-	[18] = {
+    },
+    [18] = {
       name = "HyperspaceGateTech",
       progress = 1,
     },
-	[19] = {
+    [19] = {
       name = "RepairAbility",
       progress = 1,
     },
-	[20] = {
+    [20] = {
       name = "ScoutEMPAbility",
       progress = 1,
     },
-	[21] = {
+    [21] = {
       name = "BattleshipHullEngineering",
       progress = 1,
     },
-   },
+  },
 }
-

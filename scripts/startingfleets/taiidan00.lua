6f226442f0af7d6498055f758c69a888f7afd019
Tanner Mckenney <tmckenney7@outlook.com>
2023-04-11 20:37:09 -0700

    Update Taiidan starting fleets

---
+++ scripts/startingfleets/taiidan00.lua
@@ -0,0 +1,254 @@
+PersistantData = {
+  StrikeGroups = {
+  },
+  Squadrons = {
+    -- Flagship
+    {
+      type = "tai_MotherShip",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+    },
+    -- Carrier
+    {
+      type = "tai_Carrier",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+    -- Utility
+    {
+      type = "tai_ResourceCollector",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 6,
+    },
+    {
+      type = "tai_ResourceController",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+    {
+      type = "tai_ResearchShip",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+    {
+      type = "tai_SensorArray",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+    {
+      type = "tai_Cloakgenerator",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+    {
+      type = "tai_gravwellgenerator",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+    -- Super-Caps
+    {
+      type = "tai_HeavyCruiser",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 2,
+    },
+    {
+      type = "tai_Destroyer",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 3,
+    },
+    {
+      type = "tai_MissileDestroyer",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 3,
+    },
+    -- Frigates
+    {
+      type = "tai_AssaultFrigate",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 5,
+    },
+    {
+      type = "tai_IonCannonFrigate",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 5,
+    },
+    {
+      type = "tai_FieldFrigate",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+    {
+      type = "tai_SupportFrigate",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+    -- Corvettes
+    {
+      type = "tai_repaircorvette",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      number = 5,
+    },
+    {
+      type = "tai_salvagecorvette",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      number = 5,
+    },
+    {
+      type = "tai_lightcorvette",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      number = 15,
+    },
+    {
+      type = "tai_heavycorvette",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      number = 10,
+    },
+    {
+      type = "tai_multiguncorvette",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      number = 10,
+    },
+    -- Fighters
+    {
+      type = "tai_scout",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      number = 5,
+    },
+    {
+      type = "tai_interceptor",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      number = 20,
+    },
+    {
+      type = "tai_attackbomber",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      number = 20,
+    },
+    {
+      type = "tai_defensefighter",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      number = 15,
+    },
+    {
+      type = "tai_defender",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      number = 15,
+    },
+  },
+  Research = {
+  },
+}
